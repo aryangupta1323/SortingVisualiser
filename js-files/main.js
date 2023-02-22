927b0f32e170a357bfe903bc6b69aea90d62f1be
Aryan Gupta <guptaaryan1323@gmail.com>
2023-02-22 13:28:20 +0530

    added responsiveness

--- js-files/main.js
+++ js-files/main.js
@@ -1,44 +1,50 @@
-function newArray(size){
-    const container=document.getElementById('container')
-    container.textContent=''
-    let arr=[]
-    for(let i=0;i<size;i++){
-        arr.push(Math.random()*size)
-    }
-
-    
-
-    for(let i=0;i<size;i++){
-        const ele=document.createElement('div')
-        ele.style.height=`${4000*arr[i]%400}px`
-        ele.classList.add('ele')
-        ele.setAttribute("id", `ele${i+1}`)
-        container.appendChild(ele)
-    }
+function newArray(size, modu) {
+	const container = document.getElementById("container");
+	container.textContent = "";
+	let arr = [];
+	for (let i = 0; i < size; i++) {
+		arr.push(Math.random() * size);
+	}
+	for (let i = 0; i < size; i++) {
+		const ele = document.createElement("div");
+		ele.style.height = `${(4000 * arr[i]) % modu}px`;
+		ele.classList.add("ele");
+		ele.setAttribute("id", `ele${i + 1}`);
+		container.appendChild(ele);
+	}
 }
-function waitforme(ms) { 
-    return new Promise(resolve => { 
-        setTimeout(() => { resolve('') }, ms); 
-    }) 
+function waitforme(ms) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("");
+		}, ms);
+	});
 }
 
 let delay = 260;
-let delayElement = document.querySelector('#speed');
+let delayElement = document.querySelector("#speed");
 
-// Event listener to update delay time 
-delayElement.addEventListener('input', function(){
-    console.log(delayElement.value, typeof(delayElement.value));
-    delay = 140 - parseInt(delayElement.value);
+// Event listener to update delay time
+delayElement.addEventListener("input", function () {
+	console.log(delayElement.value, typeof delayElement.value);
+	delay = 140 - parseInt(delayElement.value);
 });
 
 // no of bars updation
 
-let noOfBars=document.querySelector('#noBars')
-noOfBars.addEventListener('input',()=>{
-    newArray(noOfBars.value)
-})
+let noOfBars = document.querySelector("#noBars");
+noOfBars.addEventListener("input", () => {
+	newArray(noOfBars.value);
+});
 
-newArray(100)
-document.getElementById('new_array').addEventListener('click',()=>{
-    newArray(noOfBars.value)
-})
+if (screen.width < 500) {
+	newArray(30, 350);
+	document.getElementById("noBars").value = 30;
+	document.getElementById("noBars").max = 30;
+} else {
+	newArray(60, 400);
+	document.getElementById("noBars").value = 60;
+}
+document.getElementById("new_array").addEventListener("click", () => {
+	newArray(noOfBars.value);
+});
